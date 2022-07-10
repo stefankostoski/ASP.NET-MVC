34994f9a72c691116f30d3e5ca643ec5d8527286
Stefan Kostoski <72026527+stefankostoski@users.noreply.github.com>
2022-07-11 00:04:17 +0200

    Update Program.cs

--- BurgerWebApp/BurgerWebApp/Program.cs
+++ BurgerWebApp/BurgerWebApp/Program.cs
@@ -3,6 +3,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+//builder.Services.AddTransient<IBurgerService, BurgerService>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
